026a5cc8ab1adea4c96ed6fef15bd53014bc479d
Matteosalvatori4 <matteo.salvatori4@gmail.com>
2021-04-16 18:22:46 +0200

    Creazione lato server e application properties #1

---
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/AmmazzonserverApplication.java
@@ -0,0 +1,13 @@
+package it.iad2.ammazzonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmmazzonserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmmazzonserverApplication.class, args);
+	}
+
+}
