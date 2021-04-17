01829088fcaea190bdd50de526b64279b66ecf0f
Angelita Doria <angiegiuse@gmail.com>
2021-04-17 12:39:54 +0200

    Commit parziale ticket #9
    Implementato package service

---
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/service/AssociaTaglieColoriProdService.java
@@ -0,0 +1,86 @@
+package it.iad2.ammazzonserver.service;
+
+import it.iad2.ammazzonserver.dto.ListaProdottiColoriDto;
+import it.iad2.ammazzonserver.dto.ListaProdottiDto;
+import it.iad2.ammazzonserver.dto.ListaTaglieDto;
+import it.iad2.ammazzonserver.model.ColoreTaglia;
+import it.iad2.ammazzonserver.model.Prodotto;
+import it.iad2.ammazzonserver.model.ProdottoColore;
+
+public interface AssociaTaglieColoriProdService {
+    /**
+     * Consente una ricerca per codice esatto o per descrizione like dei prodotti
+     * presenti in banca dati
+     * @param criterio di tipo String, parametro di ricerca
+     * @return ListaProdottiDto classe che contiene una lista di
+     * @see Prodotto
+     */
+    ListaProdottiDto cercaProdottiPerCodiceDescrizione(String criterio);
+    
+    /**
+     * Mostra una lista di colori associati al prodotto selezionato
+     * @param prodotto di tipo Prodotto
+     * @see Prodotto
+     * @return ListaProdottiColoriDto classe che contiene una lista di
+     * @see ProdottoColore
+     */
+    ListaProdottiColoriDto mostraColoriAssociatiAProdotto(Prodotto prodotto);
+    
+    /**
+     * Mostra una lista di taglie associate al prodotto-colore selezionato
+     * @param prodColore criterio di ricerca di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto mostraTaglieAssociateAProdottoColore(ProdottoColore prodColore);
+    
+    /**
+     * Mostra una lista di taglie non associate al prodotto-colore selezionato
+     * @param prodColore criterio di ricerca di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto mostraTaglieNonAssociateAProdottoColore(ProdottoColore prodColore);
+    
+    /**
+     * Disassocia la taglia selezionata dal ProdottoColore
+     * @param taglia di tipo
+     * @see Taglia
+     * @param prodColore di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto rimuoviTaglia(ColoreTaglia taglia, ProdottoColore prodColore);
+    
+    /**
+     * Associa la taglia selezionata al ProdottoColore
+     * @param taglia di tipo
+     * @see Taglia
+     * @param prodColore di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto associaTaglia(ColoreTaglia taglia, ProdottoColore prodColore);
+    
+    /**
+     * Associa tutte le taglie al ProdottoColore
+     * @param prodColore di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto associaTutteLeTaglie(ProdottoColore prodColore);
+    
+    /**
+     * Disassocia tutte le taglie al ProdottoColore
+     * @param prodColore di tipo
+     * @see ProdottoColore
+     * @return ListaTaglieDto classe che contiene una lista di
+     * @see ColoreTaglia
+     */
+    ListaTaglieDto disassociaTutteLeTaglie(ProdottoColore prodColore);
+}
