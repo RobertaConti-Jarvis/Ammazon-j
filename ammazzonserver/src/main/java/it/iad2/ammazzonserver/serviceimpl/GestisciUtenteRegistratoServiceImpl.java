c0c58f4d82accfcd3edd81eec9db0489033eca66
DESKTOP-J5C4BKM\Samuele <5>
2021-04-19 17:00:26 +0200

    implementati service e serviceImpl #5

---
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/serviceimpl/GestisciUtenteRegistratoServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.iad2.ammazzonserver.serviceimpl;
+
+import it.iad2.ammazzonserver.model.UtenteRegistrato;
+import it.iad2.ammazzonserver.repository.UtenteRegistratoRepository;
+import it.iad2.ammazzonserver.service.GestisciUtenteRegistratoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Samuele
+ */
+@Service
+public class GestisciUtenteRegistratoServiceImpl implements GestisciUtenteRegistratoService {
+
+    @Autowired
+    UtenteRegistratoRepository utenteRegistratoRepository;
+
+    @Override
+    public List<UtenteRegistrato> cercaUtenteRegistrato(String criterio) {
+        return utenteRegistratoRepository.findByUsernameContains(criterio);
+    }
+
+    @Override
+    public List<UtenteRegistrato> modificaUtenteRegistrato(UtenteRegistrato utente) {
+        utenteRegistratoRepository.save(utente);
+        return aggiorna();
+    }
+
+    @Override
+    public List<UtenteRegistrato> rimuoviUtenteRegistrato(UtenteRegistrato utente) {
+        utenteRegistratoRepository.delete(utente);
+        return aggiorna();
+    }
+
+    @Override
+    public List<UtenteRegistrato> aggiorna() {
+        return utenteRegistratoRepository.findAll();
+    }
+
+    @Override
+    public List<UtenteRegistrato> aggiungiUtenteRegistrato(UtenteRegistrato utente) {
+        utenteRegistratoRepository.save(utente);
+        return aggiorna();
+    }
+
+}
