10d432379f359915b5c97e6c406e25ad236dc6a6
mainieri@tm <mainieri.tm@programmer.net>
2021-04-19 17:42:09 +0200

    Update GestioneTaglieServiceImpl.java

--- ammazzonserver/src/main/java/it/iad2/ammazzonserver/serviceimpl/GestioneTaglieServiceImpl.java
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/serviceimpl/GestioneTaglieServiceImpl.java
@@ -1,6 +1,10 @@
 package it.iad2.ammazzonserver.serviceimpl;
 
+import it.iad2.ammazzonserver.model.ColoreTaglia;
+import it.iad2.ammazzonserver.model.ProdottoColore;
 import it.iad2.ammazzonserver.model.VarianteTaglia;
+import it.iad2.ammazzonserver.repository.ColoreTagliaRepository;
+import it.iad2.ammazzonserver.repository.ProdottoColoreRepository;
 import it.iad2.ammazzonserver.repository.VarianteTagliaRepository;
 import it.iad2.ammazzonserver.service.GestioneTaglieService;
 import java.util.List;
@@ -12,6 +16,12 @@ public class GestioneTaglieServiceImpl implements GestioneTaglieService {
 
     @Autowired
     VarianteTagliaRepository varianteTagliaRepository;
+
+    @Autowired
+    ColoreTagliaRepository coloreTagliaRepository;
+
+    @Autowired
+    ProdottoColoreRepository prodottoColoreRepository;
 
     @Override
     public List<VarianteTaglia> ricerca(String criterio) {
@@ -32,6 +42,21 @@ public class GestioneTaglieServiceImpl implements GestioneTaglieService {
 
     @Override
     public List<VarianteTaglia> rimuovi(VarianteTaglia taglia) {
+        List<ColoreTaglia> coloriTaglia = taglia.getListaColoreTaglia();
+//      prendi prodotto colore
+        coloriTaglia.forEach(r -> {
+            ProdottoColore pc = r.getProdottoColore();
+            pc.getListaColoreTaglia().removeIf((t) -> t.getId().equals(r.getId()));
+            prodottoColoreRepository.save(pc);
+        });
+//      rimuovi taglia da prodotto colore
+
+        coloriTaglia.forEach(r -> {
+            VarianteTaglia vt = r.getVarianteTaglia();
+            vt.getListaColoreTaglia().removeIf((t) -> t.getId().equals(r.getId()));
+            varianteTagliaRepository.save(vt);
+            coloreTagliaRepository.delete(r);
+        });
         varianteTagliaRepository.delete(taglia);
         return aggiorna();
     }
