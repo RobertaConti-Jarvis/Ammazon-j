c1e53d875d71ee50b387d8e56e322f6698ccf1cf
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-04-20 17:23:34 +0200

    #8

--- ammazzonserver/src/main/java/it/iad2/ammazzonserver/repository/ProdottoColoreRepository.java
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/repository/ProdottoColoreRepository.java
@@ -1,6 +1,7 @@
 package it.iad2.ammazzonserver.repository;
 
 import it.iad2.ammazzonserver.model.ProdottoColore;
+import it.iad2.ammazzonserver.model.VarianteColore;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,11 +9,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdottoColoreRepository extends JpaRepository<ProdottoColore, Long>{
-    
+public interface ProdottoColoreRepository extends JpaRepository<ProdottoColore, Long> {
+
     @Query("select pc from ProdottoColore pc"
             + " join pc.varianteColore c"
             + " join pc.prodotto p"
             + " where p.id = :id")
     List<ProdottoColore> trovaListaVarianteColoreProdotto(@Param("id") Long id);
+
+    @Query("select vc from Prodotto p"
+           + " join p.listaProdottoColore pc"
+            + " join pc.varianteColore vc"
+            + " where p.id=:id")
+    List<VarianteColore> selezionaColoriAssociatiProdotto(@Param("id") Long id);
+
 }
