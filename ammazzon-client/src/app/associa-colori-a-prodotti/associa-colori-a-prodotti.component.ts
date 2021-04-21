20a3736a7fa25adb5fec5de068f3c4b5bf37bb47
AlbertoMartinezMontero <79788629+AlbertoMartinezMontero@users.noreply.github.com>
2021-04-21 10:56:58 +0200

    #3
    inserimento css

--- ammazzon-client/src/app/associa-colori-a-prodotti/associa-colori-a-prodotti.component.ts
+++ ammazzon-client/src/app/associa-colori-a-prodotti/associa-colori-a-prodotti.component.ts
@@ -12,7 +12,7 @@ import { VarianteColore } from '../entità/variante-colore';
 @Component({
   selector: 'app-associa-colori-a-prodotti',
   templateUrl: './associa-colori-a-prodotti.component.html',
-  styleUrls: ['./associa-colori-a-prodotti.component.css']
+  styleUrls: ['../theme.css'],
 })
 export class AssociaColoriAProdottiComponent implements OnInit {
 
