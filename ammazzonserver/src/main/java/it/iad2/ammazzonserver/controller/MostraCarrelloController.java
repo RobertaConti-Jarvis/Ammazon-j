890313f23da7f56e0222316ac5458f03a46c9e2d
gianni.golia <gianni.golia@gmail.com>
2021-04-22 16:17:56 +0200

    Risolto Bug Server

--- ammazzonserver/src/main/java/it/iad2/ammazzonserver/controller/MostraCarrelloController.java
+++ ammazzonserver/src/main/java/it/iad2/ammazzonserver/controller/MostraCarrelloController.java
@@ -5,6 +5,7 @@
  */
 package it.iad2.ammazzonserver.controller;
 
+import org.springframework.web.bind.annotation.RestController;
 import it.iad2.ammazzonserver.dto.DatiOrdinePageDto;
 import it.iad2.ammazzonserver.dto.PageDto;
 import it.iad2.ammazzonserver.service.MostraCarrelloService;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  *
